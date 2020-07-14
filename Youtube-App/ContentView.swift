4dbf894f04a70da8b0b4fce4da2872d8d63f9854
kothari <kothari@Macbook.local>
2020-07-14 17:23:31 +0530

    initial xcode project creation

---
+++ Youtube-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Youtube-App
+//
+//  Created by kothari on 14/07/20.
+//  Copyright © 2020 kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
